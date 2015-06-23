e5df3e7792c189b9a1efcfb98aa7fc122eb43b54
theo-l <theol.liang@gmail.com>
2015-06-22 21:50:27 -0300

    updated

--- j2ee/java_single_file_parser.py
+++ j2ee/java_single_file_parser.py
@@ -2,54 +2,14 @@
 # -*- encoding=utf8 -*-
 
 keywords=[
-        "package",
-        "import",
-        "public",
-        "private",
-        "protected",
-        "abstract",
-        "class",
-        "interface",
-        "extends",
-        "implements",
-        "for",
-        "while",
-        "do",
-        "if",
-        "else",
-        "switch",
-        "case",
-        "try",
-        "catch",
-        "final",
-        "finally",
-        "throw",
-        "throws",
-        "assert",
-        "this",
-        "super",
-        "volatile",
-        "synchronized",
-        "return",
-        "break",
-        "continue",
-        "new",
-        "instanceof",
-        "native",
-        "static",
-        "enum",
-        "boolean",
-        "byte",
-        "char",
-        "default",
-        "double",
-        "float",
-        "int",
-        "long",
-        "short",
-        "void",
-        "transient",
-        "strictfp"
+        "package", "import", "public", "private", "protected", "abstract",
+        "class", "interface", "extends", "implements", "for", "while",
+        "do", "if", "else", "switch", "case", "try",
+        "catch", "final", "finally", "throw", "throws", "assert",
+        "this", "super", "volatile", "synchronized", "return", "break",
+        "continue", "new", "instanceof", "native", "static", "enum",
+        "boolean", "byte", "char", "default", "double", "float",
+        "int", "long", "short", "void", "transient", "strictfp"
         ]
 
 tokenType=[
@@ -65,5 +25,18 @@ tokenType=[
        "SWITCHBLOCK",
        "FORBLOCK",
         ]
+replaceChars=[
+        "(", ")", "{", "}", ";", ":", ",", "\""      
+        ]
 
 print len(keywords)
+
+java_file=open("./ClientPortfolioManagerBean.java","r")
+
+for line in java_file:
+    for char in replaceChars:
+        line.replace(char," "+char+" ")
+
+    for token in line.split(" "):
+        print token,"\n"
+
